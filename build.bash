19425bc62d3c4839d0cf3e0251557e77403b0e08
Connor Bailey <connorbailey@CB-iMac.local>
2023-01-08 13:58:11 -0800

    add echos to build script

--- build.bash
+++ build.bash
@@ -1,6 +1,11 @@
+echo "building..."
 ! test -d "dist" && mkdir dist
+
+echo "dist directory exists. copying html and css"
 
 cp "public/index.html" "dist"
 cp "public/styles.css" "dist"
+echo "html and css successfully copied. running webpack..."
 
-webpack --watch
+webpack
+echo "webpack completed"
